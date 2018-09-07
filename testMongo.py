53db508e9b45330c3158c3024d915abe22dfd7f2
yamaha6297 <38706103+yamaha6297@users.noreply.github.com>
2018-09-07 15:38:15 -0300

    Update testMongo.py

--- testMongo.py
+++ testMongo.py
@@ -2,10 +2,9 @@
 import pymongo
 from pymongo import MongoClient
 import random
-from datetime import time
-from random import sample
-from string import digits, ascii_letters
+from datetime import datetime
 from avgFunc import avg
+from listMongo import list
 
 #MongoClient to running mongod instance
 client = MongoClient()
@@ -17,16 +16,11 @@ db = client.test
 collection = db.person
 
 times = []
-
-for i in range(0,10):
-    d1=time.now()
-    #insert 1000000 records
-    for n in range(0,1000000):
-        chars = digits + ascii_letters
-        cadena = ''.join(sample(chars, 8))
-        #(dictionary of data) data to insert
-        data_id = collection.insert_one({"nick": cadena,
-                                        "age":random.randint(18,90)}).inserted_id
+#insert 1000000 records
+for n in range(0,10):
+    d1=datetime.now()
+    #(dictionary of data) data to insert
+    data_id = collection.insert_many(list()).inserted_ids
     time = datetime.now() - d1
     print (time)
     times.append(time)
